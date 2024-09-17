1035789d55dde0c2242035fef8e5ff4aceaa32bc
lyricall <lachariotte30@gmail.com>
2024-09-17 11:53:35 +0200

    C00

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 11:02:43 by agraille          #+#    #+#             */
+/*   Updated: 2024/09/17 11:09:51 by agraille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
