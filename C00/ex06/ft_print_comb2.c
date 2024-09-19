2cebc1f8750c7a98f1cb5619f123c83d13c5590b
lyricall <lachariotte30@gmail.com>
2024-09-19 09:45:12 +0200

    C00

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,9 +6,44 @@
 /*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 11:03:54 by agraille          #+#    #+#             */
-/*   Updated: 2024/09/19 08:09:37 by agraille         ###   ########.fr       */
+/*   Updated: 2024/09/19 09:15:04 by agraille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char s)
+{
+	write(1, &s, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <=98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if (a != 98)
+				write(1, ", ", 2);
+
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
