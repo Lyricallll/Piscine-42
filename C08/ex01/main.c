acbfdf94f2ac2527fa3d6964d2f555e00e892909
lyricallll <lachariotte30@gmail.com>
2024-11-05 07:15:44 +0100

    42

---
+++ C08/ex01/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 07:10:47 by agraille          #+#    #+#             */
+/*   Updated: 2024/09/30 09:54:01 by agraille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_boolean.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+t_bool	ft_is_even(int nbr)
+{
+	return ((EVEN(nbr)) ? TRUE : FALSE);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
+}
